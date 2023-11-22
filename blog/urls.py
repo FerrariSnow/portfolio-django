c0a2cea01adec6cc803a046fbdbff8a912dd5e92
Ferrari <ferraridev18@gmail.com>
2023-11-22 17:07:05 -0300

    ajustando organização de arquivos e pastas

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-
 from blog import views
 
+from .views import (
+    PostView,
+    PostDetail
+)
+
 urlpatterns = [
-    path('', views.PostView.as_view(), name='home')
+    path('', views.PostView.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views/post_view.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views import generic
-
-
-class PostView(generic.View):
-    def get(self, request, *args, **kwargs):
-        # return HttpResponse('Hello World !')
-        return render(request, "base.html")
